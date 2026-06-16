b60170a6aa040dea4fea94f01f41beee84390138
Hussein-kadhim <h-kadhim@outlook.com>
2026-06-16 10:02:46 +0200

    Database foutafhandeling verbeterd

--- account-registratie/account-beheren/index.php
+++ account-registratie/account-beheren/index.php
@@ -1,4 +1,9 @@
 <?php
+// Sta toe dat de applicatie de DB fout opvangt
+if (!defined('ALLOW_DB_FAILURE')) {
+    define('ALLOW_DB_FAILURE', true);
+}
+
 // Laad database configuratie
 require_once __DIR__ . '/../../config.php';
 
