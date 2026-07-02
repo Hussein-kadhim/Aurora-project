ca29498442f329a6d1b13e2da4dd6a34845fafc6
Ayssar801 <344016@student.mboutrecht.nl>
2026-07-02 16:16:45 +0200

    opslaan en annuleren gemaakt

--- medewerker-registratie/medewerker-beheren/views/edit.php
+++ medewerker-registratie/medewerker-beheren/views/edit.php
@@ -187,3 +187,23 @@ $opmerking       = $opmerking ?? '';
                             ><?= htmlspecialchars($opmerking) ?></textarea>
                         </div>
                     </div>
+
+                    <!-- Knoppen -->
+                    <div class="form-actions">
+                        <button type="submit" class="btn-primary">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" style="width: 18px; height: 18px; margin-right: 6px; vertical-align: middle;">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
+                            </svg>
+                            Opslaan
+                        </button>
+                        <a href="index.php" class="btn-secondary">Annuleren</a>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+    </main>
+
+    <?php require_once __DIR__ . '/../../../includes/footer.php'; ?>
+</body>
+</html>
