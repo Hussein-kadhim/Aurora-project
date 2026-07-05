37c9583491dc6693862ccb5b2413d9d4b4a87fb5
Salmany16 <344901@student.mboutrecht.nl>
2026-07-05 14:10:50 +0200

    feat(view): verstuur-icoon per rij en status Verzonden toegevoegd aan overzicht

--- melding/views/meldingen.php
+++ melding/views/meldingen.php
@@ -197,18 +197,20 @@
                                                     Ongelezen
                                                 </span>
                                             <?php else: ?>
-                                                <span class="status-indicator status-read">
+                                                <span class="status-indicator status-verzonden">
                                                     <span class="status-dot"></span>
-                                                    Gelezen
+                                                    Verzonden
                                                 </span>
                                             <?php endif; ?>
                                         </td>
                                         <td class="actions-cell">
-                                            <a href="#" class="action-btn" title="Melding openen"
-                                                onclick="alert('Meldingdetails bekijken is nog in ontwikkeling.'); return false;">
+                                            <a href="meldingen.php?action=verstuur&id=<?= (int) $row['Id'] ?>"
+                                               class="action-btn action-btn-verstuur"
+                                               title="Melding versturen"
+                                               id="verstuur-btn-<?= (int) $row['Id'] ?>">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                        d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                                                    <line x1="22" y1="2" x2="11" y2="13"></line>
+                                                    <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
                                                 </svg>
                                             </a>
                                         </td>
